7752380b08a0bd1151ad6bcbbd7ce85119ebc288
mta <m.tamazlicaru1959@gmail.com>
2026-08-28 15:32:02 +0300

    adauagre details email

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,9 +1,11 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { InboxComponent } from '../pages/inbox/inbox.component';
+import { MailDetailComponent } from '../pages/mail-detail/mail-detail.component';
 
 export const routes: Routes = [
 
     { path: '', redirectTo: 'inbox', pathMatch: 'full' },
     { path: 'inbox', component: InboxComponent },
+    { path: 'mail/:id', component: MailDetailComponent },
 ];
